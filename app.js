314be5f2bab00330acf8f051773f0a12466fb9e2
Almaz <86904808+almisito@users.noreply.github.com>
2024-10-26 23:07:03 -0500

    connection limit added

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectionLimit: 10,
 });
 
 connection.connect((err) => {
